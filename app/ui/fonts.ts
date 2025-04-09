4e741e4f383d980e4c172a90b56fe5818f929510
MatiasMagliano <matias.magliano@mi.unc.edu.ar>
2025-04-09 19:33:39 -0300

    Commit inicial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Carlito } from 'next/font/google';
+import { Lusitana} from 'next/font/google';
+
+export const carlito = Carlito({ 
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
